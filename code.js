abc1f15198b9171925bcbd5e76eb1a6db02de9f6
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-19 20:26:30 -0300

    HTML and CSS base version finished

---
+++ code.js
@@ -0,0 +1,47 @@
+/*
+const add = function(numOne,numTwo) {
+    if (numOne == 0 && numTwo == 0){
+      return 0
+    } else{
+      return (numOne + numTwo)
+    }
+  };
+  
+  const subtract = function(numBig,numSmall) {
+      return (numBig -numSmall)
+  };
+  
+  const sum = function(array) {
+      let instSum = 0;
+    for ( const item of array ){
+      instSum = instSum + item;
+    }
+    return instSum
+  };
+  
+  const multiply = function(array) {
+    let instMult = 1;
+    for ( const item of array ){
+      if (item == 0) {
+        return 0
+      }
+      instMult = (instMult * item);
+    }
+    return instMult
+  };
+  
+  const power = function(num, power) {
+      return (num**power)
+  };
+  
+  const factorial = function(num) {
+      let instFact = 1;
+    if ((num == 0)|| (num==1)){
+      return instFact
+    }
+    for (let i = num; i>=1; i--){
+      instFact = instFact * i;
+    }
+    return instFact
+  };
+  */
